7ec1228cf4c4755b09c767aa41ea6ec16240d875
baselaljman <baselaljman@gmail.com>
2026-03-07 09:42:39 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alaujan.app',
+  appName: 'alaujan',
+  webDir: 'public'
+};
+
+export default config;
