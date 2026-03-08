f4c445227036b2034e6803db28118a20521092f3
Basel Al3jman <baselaljman@gmail.com>
2026-03-08 03:29:16 +0000

    C:\alaujan-main>npx cap add android √ Adding native android project in a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.alaujan.app',
   appName: 'alaujan',
-  webDir: 'public'
+  webDir: 'out' // تم التغيير من public إلى out لتتوافق مع Next.js Static Export
 };
 
 export default config;
